15ee8e1642635237195be1aab1c7a9153a27001f
Kausar-1 <kausar73668@gmail.com>
2024-12-07 19:17:23 +0530

    adding files

---
+++ findFactorialOfUserInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class findFactorialOfUserInput {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int num, fact=1;
+        System.out.println("Enter a number : ");
+        num= s.nextInt();
+        while(num>1){
+
+            fact *=num;
+            num--;
+        }
+        System.out.println("The factorial of the given number is : "+fact);
+    }
+}
