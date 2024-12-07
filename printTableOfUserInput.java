84b5ac149ef0300435849ea8ffca5d9219b3fdfe
Kausar-1 <kausar73668@gmail.com>
2024-12-07 19:37:48 +0530

    adding files

---
+++ printTableOfUserInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class printTableOfUserInput {
+    public static void main (String args[]){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number for table : ");
+        int N = s.nextInt();
+        System.out.println("The tabel of "+N +":");
+        for(int i=1;i<=10;i++){
+            System.out.println(N+"*" +i+ "= " + N*i);
+        }
+    }
+}
