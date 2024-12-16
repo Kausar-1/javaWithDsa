f4451b7f81d0bf1cf9d29e7124024f7990e96260
Kausar-1 <kausar73668@gmail.com>
2024-12-16 19:34:18 +0530

    Adding Average of three numbers file.

---
+++ averageOfThreeNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class averageOfThreeNumbers {
+    public static double avgOfThreeNum(double a, double b, double c){
+        double sum = a + b + c;
+        double avg = sum / 3;
+        return avg;
+    }
+    public static void main (String args[]){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter three numbers : ");
+        Double a = s.nextDouble();
+        Double b = s.nextDouble();
+        Double c = s.nextDouble();
+        System.out.println("Average of "+a +", "+b+", "+c+ " is : "+avgOfThreeNum(a,b,c));
+        
+    }
+}
